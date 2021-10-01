9fccc07910ed6873d267ed4832236c8f1061b2a3
Michal Pawel Wadowski <80638034+Michau94@users.noreply.github.com>
2021-10-01 21:47:55 +0200

    single.blade updated

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,28 +1,27 @@
 <header>
     <div class="container">
 
-    
-            <figure>
-                <img src={{asset('images/dc-logo.png')}} alt="Logo" />
-            </figure>
-    
-            <nav>
-                <ul>
-                    <li><a href="{{url('/')}}">Characters</a></li>
-                    <li><a href="{{route('comics')}}">Comics</a></li>
-                    <li><a :href="">Movies</a></li>
-                    <li><a :href="">Tv</a></li>
-                    <li><a :href="">Games</a></li>
-                    <li><a :href="">Collectibles</a></li>
-                    <li><a :href="">Videos</a></li>
-                    <li><a :href="">Fans</a></li>
-                    <li><a :href="">News</a></li>
-                    <li><a :href="">Shop</a></li>
-                </ul>
-            </nav>
-            
-            <input type="search" placeholder="Search">
+
+        <figure>
+            <img src={{ asset('images/dc-logo.png') }} alt="Logo" />
+        </figure>
+
+        <nav>
+            <ul>
+                <li><a href="{{ url('/') }}">Characters</a></li>
+                <li><a href="{{ route('comics') }}">Comics</a></li>
+                <li><a :href="">Movies</a></li>
+                <li><a :href="">Tv</a></li>
+                <li><a :href="">Games</a></li>
+                <li><a :href="">Collectibles</a></li>
+                <li><a :href="">Videos</a></li>
+                <li><a :href="">Fans</a></li>
+                <li><a :href="">News</a></li>
+                <li><a :href="">Shop</a></li>
+            </ul>
+        </nav>
+
+        <input type="search" placeholder="Search">
 
     </div>
 </header>
-
--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -1,35 +1,88 @@
 @extends ('./layouts/main_layout');
 
 @section('content')
-    <section id="single">
-
-
-        <div class="photo-strip">
-            <div class="container">
-                <img src="{{ $comics[0]['thumb'] }}" alt="Foto">
-            </div>
-        </div>
-
-        <div class="comics-info container">
-            <div class="infos">
-                <h1>{{ $comics[0]['title'] }}</h1>
-                <div class="price">
-                    <div>US: price {{ $comics[0]['price'] }}</div>
+    <div id="single">
+        <section class="info-thumb">
+            <div class="photo-strip">
+                <div class="container">
+                    <img src="{{ $comics[0]['thumb'] }}" alt="Foto">
                 </div>
-                <p>{{ $comics[0]['description'] }}</p>
             </div>
-            <div class="adv">
-                <img src="{{ asset('../images/adv.jpg') }}" alt="Main-img">
+            <div class="comics-info container">
+                <div class="infos">
+                    <h1>{{ $comics[0]['title'] }}</h1>
+                    <div class="price">
+                        <div class="">US: price {{ $comics[0]['price'] }}</div>
+                        <div class="
+                            availability">
+                            <div class="stock ">Available
+                            </div>
+                            <div class="btn-check"> Check Availability</div>
+                        </div>
+
+                    </div>
+                    <p>{{ $comics[0]['description'] }}</p>
+                </div>
+                <div class="adv">
+                    <img src="{{ asset('../images/adv.jpg') }}" alt="Main-img">
+                </div>
             </div>
-        </div>
-
-
-
-
-
-
-
-    </section>
+        </section>
+        <section class="talent-specs">
+            <div class="container">
+                <div class="content">
+                    <div class="talent col">
+                        <h1>Talent</h1>
+                        <hr />
+                        <div class="row">
+                            <h5>Art by:</h5>
+                            <p>
+                                @foreach ($comics[0]['artists'] as $artist)
+                                    {{ $artist }},@if ($loop->last)
+                                        .
+                                    @endif
+                                @endforeach
+                            </p>
+                        </div>
+                        <hr />
+                        <div class="row">
+                            <h5>Written by:</h5>
+                            <p>
+                                @foreach ($comics[0]['writers'] as $writer)
+                                    {{ $writer }}, @if ($loop->last)
+                                    @endif
+                                @endforeach
+                            </p>
+                        </div>
+                    </div>
+                    <div class="specs col container">
+                        <h1>Specs</h1>
+                        <hr />
+                        <div class="row">
+                            <h5>Series:</h5>
+                            <p>
+                                {{ $comics[0]['series'] }}
+                            </p>
+                        </div>
+                        <hr />
+                        <div class="row">
+                            <h5>U.S Price:</h5>
+                            <p>
+                                {{ $comics[0]['price'] }}
+                            </p>
+                        </div>
+                        <hr />
+                        <div class="row">
+                            <h5>On Sale Date:</h5>
+                            <p>
+                                {{ $comics[0]['sale_date'] }}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
 
 
 
