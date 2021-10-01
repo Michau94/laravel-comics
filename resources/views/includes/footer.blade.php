2dc877d2c57fbd77fc02b5f10f6d99d1a013ddab
Michal Pawel Wadowski <80638034+Michau94@users.noreply.github.com>
2021-10-01 17:19:00 +0200

    bottom footer section

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,64 +1,102 @@
 <footer>
-    <div class="container">
-      <div class="links">
-        <div class="menu">
-          <h3>
-            DC Comics
-          </h3>
-          <ul>
-            <li><a href="">Characters</a></li>
-            <li><a href="">Comics</a></li>
-            <li><a href="">Movies</a></li>
-            <li><a href="">TV</a></li>
-            <li><a href="">Games</a></li>
-            <li><a href="">Videos</a></li>
-            <li><a href="">News</a></li>
-          </ul>
-          <h3>
-            Shop
-          </h3>
-          <ul>
-            <li><a href="">Characters</a></li>
-            <li><a href="">Movies</a></li>
-          </ul>
+    <div class="footer-top">
+      <div class="container">
+        <div class="links">
+          <div class="menu">
+            <h3>
+              DC Comics
+            </h3>
+            <ul>
+              <li><a href="">Characters</a></li>
+              <li><a href="">Comics</a></li>
+              <li><a href="">Movies</a></li>
+              <li><a href="">TV</a></li>
+              <li><a href="">Games</a></li>
+              <li><a href="">Videos</a></li>
+              <li><a href="">News</a></li>
+            </ul>
+            <h3>
+              Shop
+            </h3>
+            <ul>
+              <li><a href="">Characters</a></li>
+              <li><a href="">Movies</a></li>
+            </ul>
+          </div>
+          <div class="menu">
+            <h3>
+              DC
+            </h3>
+            <ul>
+              <li><a href="">Terms of Use</a></li>
+              <li><a href="">Privacy Policy(New)</a></li>
+              <li><a href="">Ad Choices</a></li>
+              <li><a href="">Advertising</a></li>
+              <li><a href="">Jobs</a></li>
+              <li><a href="">Subscriptions</a></li>
+              <li><a href="">Talent Workshops</a></li>
+              <li><a href="">CPSC Certificates</a></li>
+              <li><a href="">Ratings</a></li>
+              <li><a href="">Shop Help</a></li>
+              <li><a href="">Contact Us</a></li>
+            </ul>
+          </div>
+          <div class="menu">
+            <h3>
+              Sites
+            </h3>
+            <ul>
+              <li><a href="">DC</a></li>
+              <li><a href="">Mad Magazine</a></li>
+              <li><a href="">DC Kids</a></li>
+              <li><a href="">DC Universe</a></li>
+              <li><a href="">DC Power Visa</a></li>
+            </ul>
+          </div>
         </div>
-        <div class="menu">
-          <h3>
-            DC
-          </h3>
-          <ul>
-            <li><a href="">Terms of Use</a></li>
-            <li><a href="">Privacy Policy(New)</a></li>
-            <li><a href="">Ad Choices</a></li>
-            <li><a href="">Advertising</a></li>
-            <li><a href="">Jobs</a></li>
-            <li><a href="">Subscriptions</a></li>
-            <li><a href="">Talent Workshops</a></li>
-            <li><a href="">CPSC Certificates</a></li>
-            <li><a href="">Ratings</a></li>
-            <li><a href="">Shop Help</a></li>
-            <li><a href="">Contact Us</a></li>
-          </ul>
-        </div>
-        <div class="menu">
-          <h3>
-            Sites
-          </h3>
-          <ul>
-            <li><a href="">DC</a></li>
-            <li><a href="">Mad Magazine</a></li>
-            <li><a href="">DC Kids</a></li>
-            <li><a href="">DC Universe</a></li>
-            <li><a href="">DC Power Visa</a></li>
-          </ul>
+         <div class="logo">
+          <img src="{{asset('/images/dc-logo-bg.png')}}" alt="Dc-logo" />
         </div>
       </div>
-
-      
-
-       <div class="logo">
-        <img src="{{asset('/images/dc-logo-bg.png')}}" alt="Dc-logo" />
-      </div> 
-      <p>credits</p>
     </div>
+
+    <div class="footer-bottom">
+      <div class="media">
+    <div class="wrapper container">
+      <div class="button">
+        <a href="">Sign-Up Now!</a>
+      </div>
+
+      <div class="social">
+        <h3>Follow Us</h3>
+
+        <div>
+          <a href=""><img src="{{asset('../images/footer-facebook.png')}}" alt="FB"/></a>
+        </div>
+        <div>
+          <a href=""
+            ><img src="{{asset('../images/footer-pinterest.png')}}" alt="Pinterest"
+          /></a>
+        </div>
+        <div>
+          <a href=""
+            ><img src="{{asset('../images/footer-periscope.png')}}" alt="Periscope"
+          /></a>
+        </div>
+        <div>
+          <a href=""
+            ><img src="{{asset('../images/footer-twitter.png')}}" alt="Twitter"
+          /></a>
+        </div>
+        <div>
+          <a href=""
+            ><img src="{{asset('../images/footer-youtube.png')}}" alt="Youtube"
+          /></a>
+        </div>
+      </div>
+    </div>
+  </div>
+    </div>
+
+    
   </footer>
