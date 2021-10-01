d2f1b05abb524586f6cfe846647a2cafcfffc18a
Michal Pawel Wadowski <80638034+Michau94@users.noreply.github.com>
2021-10-01 15:34:51 +0200

    added @section + data from comics.php config()

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,27 @@
 
 @section('content')
 
-<p>ciao</p>
+<section class="cards">
+
+    <div class="container">
+        
+        <div class="button">
+            Current Series
+        </div>
+
+        @foreach ($comics as $comic)
+        <div class="card">
+            <figure>
+                <a href="#"><img src="{{$comic['thumb']}}" /></a>
+            </figure>
+            <figcaption> {{$comic['title']}}</figcaption>
+        </div>
+        @endforeach
+  </div>
+
+
+
+</section>
 
 
 @endsection
